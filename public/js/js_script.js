02023a392b0ad8c1a7053de4e07f1d6879c18d98
nautch <lindholmcarlaxel@gmail.com>
2020-11-20 00:57:27 +0100

    Commitar, allt klart

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,3 +1,39 @@
+
+
+document.getElementById("headline").innerHTML = "Välkommen till COFF's kaffebar";
+document.getElementById("coffeeMenu").innerHTML = "Kaffemeny";
+document.getElementById("menuChoice").innerHTML = "Välj typ av kaffe:";
+
+function getDetails () { //loopar igenom alla input-fields
+    var valueArray = [];
+    var numberOfInputs = document.getElementById('form').elements.length;
+    for (var i = 0; i < numberOfInputs; i++) {
+        var x = document.getElementById('form').elements[i];
+        if (x.type!=="radio") {
+            valueArray.push(x.value);
+        }
+    }
+    valueArray.push(document.querySelector('input[type="radio"]:checked').value); // lägger till vald value från radiobuttons manuellt efter loopen
+    return valueArray;
+};
+
+function getOrder () {
+    var coffeeArray = [];
+    var checkBoxes = document.getElementsByName('checkbox');
+    for (var i = 0; i < checkBoxes.length; i++) { // Kontrollerar vilka checkboxes som är ibockade, och adderar då värdet till array!!
+        if (checkBoxes[i].checked) {
+            coffeeArray.push(checkBoxes[i].value);
+        }
+    }
+    return coffeeArray;
+};
+
+
+
+
+
+
+
 /*function coffeeItem(item, choice) {
     this.coffee = item;
     this.choice = choice;
@@ -9,10 +45,84 @@
 var drink = new coffeeItem('Cappuccino', 'Sojamjölk');
 
 console.log( drink.hotdrink() );
-*/
-var vm = new Vue({
-  el: '#js',
-  data: {
-    arbitraryVariableName: 'Välj en burgare'
 
-})
+
+Titeln på index.html
+var headline = document.createElement('h1');
+var headlineTitle = document.createTextNode("Välkommen till COFF's kaffebar");
+headline.appendChild(headlineTitle);
+document.getElementById('header').appendChild(headline);
+*/
+
+
+
+
+
+
+
+
+
+/*
+getDetails: function () { //loopar igenom alla input-fields
+    var valueArray = [];
+    var numberOfInputs = document.getElementById('form').elements.length;
+    for (var i = 0; i < numberOfInputs; i++) {
+        var x = document.getElementById('form').elements[i];
+        if (x.type!=="radio") {
+            valueArray.push(x.value + ", ");
+        }
+    }
+    valueArray.push(document.querySelector('input[type="radio"]:checked').value); // lägger till vald value från radiobuttons manuellt efter loopen
+    return valueArray;
+};
+
+getOrder: function () {
+    var coffeeArray = [];
+    var checkBoxes = document.getElementsByName('checkbox');
+    for (var i = 0; i < checkBoxes.length; i++) { // Kontrollerar vilka checkboxes som är ibockade, och adderar då värdet till array!!
+        if (checkBoxes[i].checked) {
+            coffeeArray.push(checkBoxes[i].value + ", ");
+        }
+    }
+    return coffeeArray;
+};
+*/
+
+
+
+/*
+
+
+for (i=0; i<snus.length; i++) {
+    var theProperties = document.createElement("div");
+    var theName = document.createElement("p");
+    var theNameNode = document.createTextNode(snus[i].name);
+    var theImg = document.createElement("img");
+    theImg.src = snus[i].img;
+    var theFirst = document.createElement("li");
+    var theTaste = document.createTextNode(snus[i].taste);
+    theFirst.appendChild(theTaste);
+    var theSecond = document.createElement("li");
+    var theType = document.createTextNode(snus[i].snusType);
+    theSecond.appendChild(theType);
+    var theThird = document.createElement("li");
+    var theAdditional = document.createTextNode(snus[i].additionalProperty);
+    theThird.appendChild(theAdditional);
+    var theCheckBoxText = document.createElement("label");
+    var addToOrder = document.createTextNode("Lägg till i beställning:");
+    theCheckBoxText.appendChild(addToOrder);
+    var theCheckBox = document.createElement("input");
+    theCheckBox.setAttribute("type", "checkbox");
+    theCheckBox.setAttribute("name", "snuscheckbox"); //Använd senare
+    theCheckBox.setAttribute("value", snus[i].name);
+    theCheckBoxText.appendChild(theCheckBox);
+
+    theProperties.appendChild(theNameNode);
+    theProperties.appendChild(theImg);
+    theProperties.appendChild(theFirst);
+    theProperties.appendChild(theSecond);
+    theProperties.appendChild(theThird);
+    theProperties.appendChild(theCheckBoxText);
+    document.getElementById("wrapper").appendChild(theProperties);
+}
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,55 @@
-var vm = new Vue({
-  el: '#js',
-  data: {
-    arbitraryVariableName: 'Välj en burgare'
+'use strict';
 
-})
+var socket = io();
+
+function coffeeItem(name, image, choice1, choice2, choice3, orderButton){
+    this.name = name;
+    this.image = image;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+    this.choice3 = choice3;
+    this.checkBox = checkBox;
+};
+
+var vm = new Vue({
+    el: '#vue',
+    data: {
+        coffeeList : hotDrinks,
+        orders: {},
+        customerDetails: [],
+        orderDetails: [],
+        orderNumber: 0,
+    },
+    methods: {
+        getNext: function () {
+            this.orderNumber += 1;
+            return this.orderNumber;
+        },
+        addOrder: function (event) {
+            this.orderDetails = getOrder();
+            this.customerDetails = getDetails();
+            var deliveryLocation = this.orders.T.details;
+            socket.emit("addOrder", {orderId: this.getNext(),
+                         details: deliveryLocation,
+                         orderItems: this.orderDetails,
+                         customerInformation: this.customerDetails,
+            });
+        },
+        displayOrder: function (event) {
+            var T = {
+                x: event.currentTarget.getBoundingClientRect().left,
+                y: event.currentTarget.getBoundingClientRect().top
+            };
+            this.orders = {
+                "T": {
+                  details: {
+                    x: event.clientX - 10 - T.x,
+                    y: event.clientY - 10 - T.y
+                }}
+                }
+        }
+
+    },//methods
+
+
+}); //new Vue
