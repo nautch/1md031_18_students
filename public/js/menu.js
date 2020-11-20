216a0692bd866e836547c3b871d3ea4c052a714f
nautch <lindholmcarlaxel@gmail.com>
2020-11-20 01:00:12 +0100

    Lägger till menu.js

---
+++ public/js/menu.js
@@ -0,0 +1,26 @@
+
+var hotDrinks = [
+{
+"name": "Cappuccino",
+"image": "/img/cappuccino.jpg",
+"choice1": "Sojamjölk",
+"choice2": "Havremjölk",
+"choice3": "Mandelmjölk",
+},
+
+{
+"name": "Espresso",
+"image": "/img/espresso.jpg",
+"choice1": "Enkel",
+"choice2": "Doppio",
+"choice3": "Triplo",
+},
+
+{
+"name": "Bryggkaffe",
+"image": "/img/bryggkaffe.jpg",
+"choice1": "Mellanrost",
+"choice2": "Mörkrost",
+"choice3": "Extra mörkrost",
+},
+]
